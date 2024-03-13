79e3fbb9f19edbe5cceaff5aad8841f53fa090dd
Mübarek ALBAYATI <mubapc1@gmail.com>
2024-03-14 02:04:14 +0300

    Birthday Buddy

---
+++ mini-projects/src/App.jsx
@@ -0,0 +1,13 @@
+import BirthdayBuddy from '../birthday buddy/BirthdayBuddy'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <BirthdayBuddy/>
+    </>
+  )
+}
+
+export default App
